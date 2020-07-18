d1000cdad0fa2cc0c57b1f9ccd66395e47058708
Matthew Liu <mliu78@jh.edu>
2020-07-18 13:13:19 -0400

    Small link api link fix

--- client/src/api/unstableIncomeApi.js
+++ client/src/api/unstableIncomeApi.js
@@ -1,10 +1,9 @@
 import { handleResponse, handleError } from './apiUtils';
-const baseUrl =
-  'http://localhost:' +
-  (process.env.PORT || 8081) +
-  '/api/covid-19/unstableincome/';
 // const baseUrl =
-//   'https://jhu-help.herokuapp.com/api/covid-19/unstableincome/';
+//   'http://localhost:' +
+//   (process.env.PORT || 8081) +
+//   '/api/covid-19/unstableincome/';
+const baseUrl = 'https://jhu-help.herokuapp.com/api/covid-19/unstableincome/';
 
 export async function getData() {
   return await fetch(baseUrl).then(handleResponse).catch(handleError);
