d77590638b2381014068eeaf883bd4425a7285f9
Matthew Liu <mliu78@jh.edu>
2020-07-18 14:30:03 -0400

    Update collegehelpsApi.js

--- client/src/api/collegehelpsApi.js
+++ client/src/api/collegehelpsApi.js
@@ -2,8 +2,8 @@ import { handleResponse, handleError } from './apiUtils';
 // const baseUrl =
 //   'http://localhost:' +
 //   (process.env.PORT || 8081) +
-//   '/api/maryland911alternatives/';
-const baseUrl = 'https://jhu-help.herokuapp.com/api/collegehelps';
+//   '/api/covid-19/collegehelps/';
+const baseUrl = 'https://jhu-help.herokuapp.com/api/covid-19/collegehelps';
 
 export async function getData() {
   return await fetch(baseUrl).then(handleResponse).catch(handleError);
